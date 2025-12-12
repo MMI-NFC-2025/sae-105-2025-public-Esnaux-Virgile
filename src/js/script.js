acfbbef7e0bf132ca6878f6ca00df7b5816faaf4
Esnaux-Virgile <virgile.esnaux@edu.univ-fcomte.fr>
2025-12-12 09:38:56 +0100

    Interractivité js lors la validation du formulaire de contact

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-// Menu hamburger
+// URL : toutes les pages; Interaction du menu burger du footer sur toutes les pages
 const menuBtn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
 const body = document.body;
@@ -9,7 +9,7 @@ menuBtn.addEventListener('click', function () {
     body.classList.toggle('menu-open');
 });
 
-// URL: artistes.html - Carrousel
+// URL: artistes.html; Carrousel
 const carouselEl = document.getElementById('carousel');
 const prev = document.getElementById('prevBtn');
 const next = document.getElementById('nextBtn');
@@ -23,3 +23,27 @@ if (carouselEl && prev && next) {
         carouselEl.scrollLeft += 300;
     };
 }
+
+
+// URL: contact.html; Formulaire de contact
+
+const contactForm = document.querySelector('.contact__form');
+const confirmMsg = document.getElementById('confirmation-message');
+
+if (contactForm && confirmMsg) {
+    contactForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        confirmMsg.style.display = 'flex';
+
+        confirmMsg.scrollIntoView({ behavior: 'smooth' });
+
+        contactForm.reset();
+
+        setTimeout(() => {
+            confirmMsg.style.display = 'none';
+        }, 3000);
+    });
+}
+
+
